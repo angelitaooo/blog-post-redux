3b2a659e92e7e6d6545df06cb4fa672ec35cb620
Angela Ordonez <angelitaooo@me.com>
2019-10-28 14:51:52 -0500

    install lodash, create two overfetching solutions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
  import jsonPlaceholder from '../apis/jsonPlaceholder';
+ import _ from 'lodash';
 
  //For Asynchronous action creators we need to install redux-thunk middleware
  //Redux thunk can return a Plain Object and also a Function
@@ -21,12 +22,32 @@ export const fetchPosts = () => {
         dispatch({type: 'FETCH_POSTS', payload: response.data})
     }
 }
-export const fetchUser = (id) => {
-    return async (dispatch) => {
-        const response = await jsonPlaceholder.get(`/users/${id}`);
 
-        dispatch({type: 'FETCH_USER', payload: response.data})
-    }
+
+// ********** (FIRST OPTION) FIX MULTIPLE API CALLS USING MEMOIZE FROM LODASH ***
+// we need to defined a function outside the action creator and memoized outside the action creator one time ***
+// export const fetchUser = id => dispatch => {
+//     _fetchUser(id, dispatch)
+// }
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
+
+//********* (SECOND OPTION) FIX MULTIPLE API CALLS - create fetchPostsAndUsers action*/
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) =>{
+    await dispatch(fetchPosts());
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
 }
 
 
